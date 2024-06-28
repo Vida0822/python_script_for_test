6ec6da554f7bb708be972511d401ad6f38314d78
DESKTOP-DGFPD6H\SHIN HEEMIN <sinhimin11@naver.com>
2024-06-29 00:13:05 +0900

    작업 스케쥴링

---
+++ scheduling.py
@@ -0,0 +1,12 @@
+# 작업 스케쥴링 
+# 특정 시간에 특정 작업을 실행하도록 예약하는 것이다 
+# => 주기적으로 실행해야 하는 작업(스크립트, 명령어)을 자동화 할 수 있다. 
+
+# 리눅스 -> Cron 
+import os 
+import datetime 
+
+now = datetime.datetime.now()
+print(now)
+
+
